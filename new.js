a5a9d31f480b665fc73c563b5dd93213e280c806
harshal-stack <99630886+harshal-stack@users.noreply.github.com>
2022-06-28 22:36:41 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,72 @@
+// const button = document.getElementById("plus")
+// const data = document.getElementsByClassName("accdatal");
+
+// button.onclick = function () {
+//     if (data.style.display === "none") {
+//         data.style.display = "block"
+//     }
+//     else {
+//         data.style.display = "none"
+//     }
+// }
+function show() {
+    const maharaj = document.getElementById("nana1");
+
+    if (maharaj.style.display == "none") {
+        maharaj.style.display = "block"
+    }
+    else {
+        maharaj.style.display = "none"
+    }
+}
+
+function show2() {
+
+    const show_12 = document.getElementById("nana2")
+    if (show_12.style.display == "none") {
+        show_12.style.display = "block"
+    }
+    else {
+        show_12.style.display = "none"
+    }
+}
+function show3() {
+
+    const show_12 = document.getElementById("nana3")
+    if (show_12.style.display == "none") {
+        show_12.style.display = "block"
+    }
+    else {
+        show_12.style.display = "none"
+    }
+}
+function show4() {
+
+    const show_12 = document.getElementById("nana4")
+    if (show_12.style.display == "none") {
+        show_12.style.display = "block"
+    }
+    else {
+        show_12.style.display = "none"
+    }
+}
+function show5() {
+
+    const show_12 = document.getElementById("nana5")
+    if (show_12.style.display == "none") {
+        show_12.style.display = "block"
+    }
+    else {
+        show_12.style.display = "none"
+    }
+}
+function show6() {
+
+    const show_12 = document.getElementById("nana6")
+    if (show_12.style.display == "none") {
+        show_12.style.display = "block"
+    }
+    else {
+        show_12.style.display = "none"
+    }
+}
